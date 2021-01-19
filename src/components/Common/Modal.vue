6df2cbeedfca6b1c0791491d83be8357786b7d9a
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-01-19 12:33:45 -0300

    add(modal-common): add modal common component

---
+++ src/components/Common/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <ion-modal
+            :is-open="isOpen"
+            @onDidDismiss="handleDismiss"
+        >
+            <ion-page>
+            <!-- INICIO CONTENT MODAL -->
+                <ion-header>
+                    <ion-toolbar>
+                        <ion-title>{{ title }}</ion-title>
+                        <ion-buttons slot="end">
+                            <ion-button @click="dismissModal">Cancelar</ion-button>
+                        </ion-buttons>
+                    </ion-toolbar>
+                </ion-header>
+                <ion-content :fullscreen="true">
+                    <ion-header collapse="condense">
+                        <ion-toolbar>
+                            <ion-title size="large">{{ title }}</ion-title>
+                            <ion-buttons slot="end">
+                                <ion-button @click="dismissModal">Cancelar</ion-button>
+                            </ion-buttons>
+                        </ion-toolbar>
+                    </ion-header>
+                
+                    <div class="container">
+                        <!-- COMPONENT ENTRA AQUI -->
+                        <slot></slot>
+                    </div>
+                </ion-content>
+            <!-- FIM CONTENT MODAL -->
+            </ion-page>
+        </ion-modal>
+    </div>
+</template>
+
+<script lang="ts">
+import { IonModal, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons } from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
+// import Template from "./Template.vue"
+
+export default defineComponent({
+    name: "Modal",
+    components: { IonModal, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons },
+    props: ["title"],
+    emit: [],
+    setup(props,context) {
+        const handleDismiss = () => context.emit("onDismiss", true)
+
+        const isOpen = ref(true)
+
+        const dismissModal = () => isOpen.value = false
+
+        return { handleDismiss, isOpen, dismissModal }
+    }
+})
+</script>
--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -1,5 +1,6 @@
 import ProgressBar from './ProgressBar.vue'
 import Toast from './Toast.vue'
 import Alert from './Alert.vue'
+import Modal from './Modal.vue'
 
-export { ProgressBar, Toast, Alert }
+export { ProgressBar, Toast, Alert, Modal }
