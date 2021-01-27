444e62d945a0e62dd279d71664200882c25cb6cd
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-01-27 15:02:49 -0300

    add(store): add store file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex'
 
+const user = localStorage.getItem('loggedUser')
+
 export default createStore({
   state: {
+    user: user ? JSON.parse(user) : {}
   },
   mutations: {
+    setUser(state, payload) {
+      state.user = payload
+    }
   },
   actions: {
   },
-  modules: {
+  getters: {
+    isTheUserLoggedIn: state => !!state.user.access_token,
+    getUser: state => state.user,
+    getToken: state => state.user.access_token
   }
 })
