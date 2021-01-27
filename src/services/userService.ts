12c31df5f0e0c3688d2d7fd84b8dcdd7548d06bd
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-01-27 12:35:26 -0300

    add(user-service-login): added login method in user service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,14 +2,14 @@ import axios from 'axios'
 import { handleResponse } from '@/helpers'
 import { API_URL } from "@/API_URL"
 
-function login() {
-    return handleResponse(axios.get(`${API_URL}/auth/login`, {
-      headers: {
-        Accept: "application/json",
-        ContentType: "application/json"
-      }
-    }))
-  }
+function login(user: any) {
+  return handleResponse(axios.post(`${API_URL}/auth/login`, JSON.stringify(user), {
+    headers: {
+      Accept: "application/json",
+      ContentType: "application/json"
+    }
+  }))
+}
 
 const userService = {
     login
