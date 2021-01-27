b3aa8e4bcc45ef20af0ec4d9aff9194c8bd743e7
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-01-27 10:48:54 -0300

    add(search-icon): add search icon in search component

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,18 +3,24 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>Buscar receita</ion-title>
+        <ion-buttons slot="end">
+            <ion-button @click="openModal"><ion-icon :icon="search"></ion-icon></ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Buscar receita</ion-title>
+          <ion-buttons slot="end">
+              <ion-button @click="openModal"><ion-icon :icon="search"></ion-icon></ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>
       
       <div class="container">
-        <ion-button @click="openModal = true">Digitar Ingredientes</ion-button>
-        <Modal :is-progress="isSendingRequest" title="Buscar Receita" @onDismiss="openModal = false" v-if="openModal">
+        <ion-button v-if="recipes.length == 0" @click="openModal">Digitar Ingredientes</ion-button>
+        <Modal :is-progress="isSendingRequest" title="Buscar Receita" @onDismiss="closeModal" v-if="showModal">
           <IngredientSearch @success="handleSuccess" @progress="handleProgress" />
         </Modal>
         <RecipesList v-if="recipes.length > 0" :recipeList="recipes" />
@@ -24,7 +30,8 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue'
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/vue'
+import { search } from 'ionicons/icons';
 import { IngredientSearch } from '@/components/IngredientSearch'
 import { RecipesList } from '@/components/Recipes'
 import { Modal } from '@/components/Common'
@@ -41,28 +48,41 @@ export default defineComponent({
     IngredientSearch, 
     RecipesList,
     IonButton,
+    IonButtons,
+    IonIcon,
     Modal
   },
   setup() {
     const isSendingRequest = ref(false)
     const recipes = ref<any>([])
-    const openModal = ref(true)
+    const showModal = ref(true)
 
     const handleProgress = (state: any) => {
       isSendingRequest.value = state
     }
 
     const handleSuccess = (list: any) => {
-      openModal.value = false
+      showModal.value = false
       recipes.value = list
+    }
+
+    const openModal = () => {
+      showModal.value = true
+    }
+
+    const closeModal = () => {
+      showModal.value = false
     }
 
     return {
       handleProgress,
       handleSuccess,
+      openModal,
+      closeModal,
       isSendingRequest,
       recipes,
-      openModal
+      showModal,
+      search
     }
   }
 })
