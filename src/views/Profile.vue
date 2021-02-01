d858ab983352983368cf075093d8bd23ce054312
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-01 12:27:56 -0300

    add(profile): add progress bar component in profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,7 @@
 <template>
   <ion-page>
     <ion-header>
+      <ProgressBar v-if="isProgress" />
       <ion-toolbar>
         <ion-title>Perfil</ion-title>
         <ion-buttons v-if="userIsLogged" slot="end">
@@ -19,7 +20,7 @@
       </ion-header>
       
       <div class="container">
-        <AuthCard v-if="!userIsLogged" @onAuthentication="handleAuthentication"/>
+        <AuthCard v-if="!userIsLogged" @progress="changeProgress" @onAuthentication="handleAuthentication"/>
         <div v-if="userIsLogged">
           <h1>User esta logado</h1>
         </div>
@@ -34,6 +35,7 @@ import { logOutOutline } from 'ionicons/icons';
 import { defineComponent, ref } from 'vue'
 import { AuthCard } from '@/components/Auth'
 import { useStore } from "vuex"
+import { ProgressBar } from "@/components/Common"
 
 export default  defineComponent({
   name: 'Profile',
@@ -46,11 +48,14 @@ export default  defineComponent({
     AuthCard,
     IonButtons, 
     IonButton, 
-    IonIcon
+    IonIcon,
+    ProgressBar
   },
   setup() {
     const store = useStore()
     const userIsLogged = ref(store.getters.isTheUserLoggedIn)
+    const isProgress = ref(false)
+
     const handleAuthentication = () => {
       userIsLogged.value = true
     }
@@ -60,11 +65,15 @@ export default  defineComponent({
       userIsLogged.value = false
     }
 
+    const changeProgress = (value) => isProgress.value = value
+
     return {
       handleAuthentication,
+      logout,
+      changeProgress,
       userIsLogged,
       logOutOutline,
-      logout
+      isProgress
     }
   }
 })
