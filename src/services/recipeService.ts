ac67c095dd57be64fe9441cbeb1446d2f3fae315
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-01 10:59:26 -0300

    refactor(recipe-service): remove helper function and add try catch

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -1,16 +1,15 @@
 import axios from 'axios'
-import { handleResponse } from '@/helpers'
 import { API_URL } from "@/API_URL"
 
 function searchRecipes(ingredients: string[], page = 1) {
-  return handleResponse(axios.get(`${API_URL}/recipe?page=${page}`, {
+  return axios.get(`${API_URL}/recipe?page=${page}`, {
     params: {
       ingredients: ingredients
     },
     headers: {
       Accept: "application/json"
     }
-  }))
+  })
 }
 
 const recipeService = {
