d819dc5be3a1c9c2fb0be2cd35bc19a719d086c9
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-23 20:53:38 -0300

    add(category-get-all): add get all categories method

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,14 +3,23 @@ import { API_URL } from "@/API_URL"
 
 function getCategoryById(categoryId: number) {
     return axios.get(`${API_URL}/category/${categoryId}`, {
-      headers: {
-        Accept: "application/json"
-      }
+        headers: {
+            Accept: "application/json"
+        }
     })
-  }
+}
+
+function getCategories() {
+  return axios.get(`${API_URL}/category`, {
+      headers: {
+          Accept: "application/json"
+      }
+  })
+}
 
 const categoryService = {
-    getCategoryById
+    getCategoryById,
+    getCategories
 }
 
 export default categoryService
