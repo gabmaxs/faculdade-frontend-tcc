74f1ffa725e36fb45c612b975071e49c66ca696b
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-04-15 11:18:05 -0300

    project(pwa): add pwa files and config

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,8 @@
 export interface User {
-    id: number,
-    name: string,
-    email: string,
-    culinary_level?: string,
-    gender?: string,
-    photo?: string
+    id: number;
+    name: string;
+    email: string;
+    culinary_level?: string;
+    gender?: string;
+    photo?: string;
 }
