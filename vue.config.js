72a1042fff426df20ad92853b6c1039979a010b0
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-04-15 13:02:57 -0300

    project(manifest): update manifest to images

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 module.exports = {
     pwa: {
-        name: "icook"
+        name: "icook",
+        manifestOptions: {
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget lorem vitae magna varius convallis in vitae felis. Aenean lacus dui, ullamcorper vel nibh et, condimentum feugiat augue. Maecenas ultrices lorem at maximus maximus. Sed at nunc id ante porttitor ultricies eget non libero.",
+            screenshots: [
+                {
+                    src: "./img/screenshots/example.png",
+                    type: "image/png",
+                    sizes: "541x702"
+                },
+                {
+                    src: "./img/screenshots/example.png",
+                    type: "image/png",
+                    sizes: "541x702"
+                },
+                {
+                    src: "./img/screenshots/example.png",
+                    type: "image/png",
+                    sizes: "541x702"
+                }
+            ]
+        }
     }
 }
